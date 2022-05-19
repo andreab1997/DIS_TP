9282196c085fe17ab45058537fb397ab46124326
andreab1997 <andreab1997@yahoo.it>
2022-05-19 14:49:07 +0200

    Added implementation for using grids for tilde funcs

--- TildeCoeffFunc.py
+++ TildeCoeffFunc.py
@@ -8,6 +8,7 @@ from MatchingFunc import Mbg_1, Mbg_2, Mbg_3_reg, Mbq_2, Mbq_3_reg, Mgg_1_loc, M
 import scipy.special as special
 import parameters as para
 from tools import Convolute, Convolute_matching, Convolute_plus_coeff, Convolute_plus_matching, Convolute_plus_matching_per_matching
+import Initialize as Ini
 
 def Cb1_Mbg1(z):
     TR = 1./2.
@@ -25,14 +26,16 @@ def Cg_1_til_reg(z,Q,p):
     return Cg_1_m_reg(z,Q,p) - 2 * Cb_0_loc(z,Q) * Mbg_1(z,q)
 def Cg_2_til_reg(z,Q,p):
     q = [p[0],Q] 
-    return Cg_2_m_reg(z,Q,p) - 2 * Cb_0_loc(z,Q) * (Mbg_2(z,q) - Mbg_1(z,q)*Mgg_1_loc(z,q))  -2 * np.log((Q**2)/(p[0]**2))*Cb1_Mbg1(z)
+    #return Cg_2_m_reg(z,Q,p) - 2 * Cb_0_loc(z,Q) * (Mbg_2(z,q) - Mbg_1(z,q)*Mgg_1_loc(z,q))  -2 * np.log((Q**2)/(p[0]**2))*Cb1_Mbg1(z)
+    return Ini.Cg2_til(z,Q)[0]
 def Cg_3_til_reg(z,Q,p):
     q = [p[0],Q] 
     return Cg_3_m_reg(z,Q,p) +Cg_2_m_reg(z,Q,p)*Mgg_1_loc(z,q) + P2(q)*Cg_1_m_reg(z,Q,p) - (Cg_1_m_reg(z,Q,p)*Mgg_2_loc(z,q) + Convolute(Cg_1_m_reg,Mgg_2_reg,z,Q,p) + Convolute_plus_matching(Cg_1_m_reg,Mgg_2_sing,z,Q,p)) - 2*Cb_0_loc(z,Q)*(Mbg_3_reg(z,q) - Mgg_1_loc(z,q)*Mbg_2(z,q) + Mbg_1(z,q)*Mgg_1_loc(z,q)*Mgg_1_loc(z,q) - (Mbg_1(z,q)*Mgg_2_loc(z,q) + Convolute_matching(Mbg_1,Mgg_2_reg,z,Q,p) + Convolute_plus_matching_per_matching(Mgg_2_sing,Mbg_1,z,Q,p) )) -2*(Cb_1_loc(z,Q)*Mbg_2(z,q) + Convolute(Cb_1_reg,Mbg_2,z,Q,p) + Convolute_plus_coeff(Cb_1_sing,Mbg_2,z,Q,p) - Cb1_Mbg1(z)*Mgg_1_loc(z,q)) -2*(Mbg_1(z,q)*Cb_2_loc(z,Q) + Convolute(Cb_2_reg,Mbg_1,z,Q,p) + Convolute_plus_coeff(Cb_2_sing,Mbg_1,z,Q,p)) 
 
 def Cq_2_til_reg(z,Q,p):
     q = [p[0],Q] 
-    return Cq_2_m_reg(z,Q,p) - 2 * Cb_0_loc(z,Q) * Mbq_2(z,q)
+    #return Cq_2_m_reg(z,Q,p) - 2 * Cb_0_loc(z,Q) * Mbq_2(z,q)
+    return Ini.Cq2_til(z,Q)[0]
 def Cq_3_til_reg(z,Q,p):
     q = [p[0],Q] 
     return Cq_3_m_reg(z,Q,p) + 2*Cq_2_m_reg(z,Q,p)*Mgg_1_loc(z,q) - Convolute(Cg_1_m_reg,Mgq_2_reg,z,Q,p) - 2*(Cb_1_loc(z,Q)*Mbq_2(z,q) + Convolute(Cb_1_reg,Mbq_2,z,Q,p) + Convolute_plus_coeff(Cb_1_sing,Mbq_2,z,Q,p)) -2*(Cb_0_loc(z,Q)*Mbq_3_reg(z,q))
@@ -42,14 +45,16 @@ def CLg_1_til_reg(z,Q,p):
     return CLg_1_m_reg(z,Q,p) 
 def CLg_2_til_reg(z,Q,p):
     q = [p[0],Q] 
-    return CLg_2_m_reg(z,Q,p) - 2 * np.log((Q**2)/(p[0]**2)) * CLb1_Mbg1(z)
+    #return CLg_2_m_reg(z,Q,p) - 2 * np.log((Q**2)/(p[0]**2)) * CLb1_Mbg1(z)
+    return Ini.CLg2_til(z,Q)[0]
 def CLg_3_til_reg(z,Q,p):
     q = [p[0],Q] 
     return CLg_3_m_reg(z,Q,p) +CLg_2_m_reg(z,Q,p)*Mgg_1_loc(z,q)+ P2(q)*CLg_1_m_reg(z,Q,p) - (CLg_1_m_reg(z,Q,p)*Mgg_2_loc(z,q) + Convolute(CLg_1_m_reg,Mgg_2_reg,z,Q,p) + Convolute_plus_matching(CLg_1_m_reg,Mgg_2_sing,z,Q,p)) -2*(Convolute(CLb_1_reg,Mbg_2,z,Q,p) - CLb1_Mbg1(z)*Mgg_1_loc(z,q)) -2*(CLb_2_loc(z,Q)*Mbg_1(z,q) + Convolute(CLb_2_reg,Mbg_1,z,Q,p))
 
 def CLq_2_til_reg(z,Q,p):
     q = [p[0],Q] 
-    return CLq_2_m_reg(z,Q,p) 
+    #return CLq_2_m_reg(z,Q,p) 
+    return Ini.CLq2_til(z,Q)[0]
 def CLq_3_til_reg(z,Q,p):
     q = [p[0],Q] 
     return CLq_3_m_reg(z,Q,p) + 2*CLq_2_m_reg(z,Q,p)*Mgg_1_loc(z,q) - Convolute(CLg_1_m_reg,Mgq_2_reg,z,Q,p) - 2*Convolute(CLb_1_reg,Mbq_2,z,Q,p) 
