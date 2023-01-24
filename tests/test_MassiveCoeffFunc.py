74d8f87358c5d6bd32319165b56c47e5893752d0
Giacomo Magni <giac.magni@gmail.com>
2023-01-24 09:32:13 +0100

    add massive NNLO from yadism

--- tests/test_MassiveCoeffFunc.py
+++ tests/test_MassiveCoeffFunc.py
@@ -1,8 +1,6 @@
 from numpy.testing import assert_allclose
 import numpy as np
 
-import warnings
-warnings.simplefilter(action='ignore', category=DeprecationWarning)
 from yadism.coefficient_functions.heavy import f2_nc, fl_nc
 from dis_tp import MassiveCoeffFunc as cf
 from dis_tp.parameters import charges, masses
@@ -15,11 +13,14 @@ NF = h_id
 e_h = charges(h_id)
 mhq = masses(h_id)
 p = np.array([mhq, e_h])
+use_grids = False
 
-Initialize_all(h_id)
+if use_grids:
+    Initialize_all(h_id)
+
 
 class Test_F2:
-    xs = [0.0001, 0.0123, 0.456]
+    xs = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.456, 0.7]
     Q = 10
 
     def test_nlo(self):
@@ -39,16 +40,14 @@ class Test_F2:
             f2_g = f2_nc.GluonVV(esf, NF, m2hq=mhq**2).NNLO()
             # g reg
             yad = f2_g.reg(x, f2_g.args["reg"])
-            my = cf.Cg_2_m_reg(x, self.Q, p, NF) / e_h**2
-            assert_allclose(my, yad, rtol=6e-2)
+            my = cf.Cg_2_m_reg(x, self.Q, p, NF, use_grids) / e_h**2
+            assert_allclose(my, yad)
 
             f2_s = f2_nc.SingletVV(esf, NF, m2hq=mhq**2).NNLO()
             # singlet reg
             yad = f2_s.reg(x, f2_s.args["reg"])
-            my = cf.Cq_2_m_reg(x, self.Q, p, NF) / e_h**2
-            assert_allclose(my, yad, rtol=6e-2)
-
-
+            my = cf.Cq_2_m_reg(x, self.Q, p, NF, use_grids) / e_h**2
+            assert_allclose(my, yad)
 
 
 class Test_FL:
@@ -70,12 +69,11 @@ class Test_FL:
             fl_g = fl_nc.GluonVV(esf, NF, m2hq=mhq**2).NNLO()
             # g reg
             yad = fl_g.reg(x, fl_g.args["reg"])
-            my = cf.CLg_2_m_reg(x, self.Q, p, NF) / e_h**2
-            assert_allclose(my, yad, rtol=6e-2)
+            my = cf.CLg_2_m_reg(x, self.Q, p, NF, use_grids) / e_h**2
+            assert_allclose(my, yad)
 
             fl_s = fl_nc.SingletVV(esf, NF, m2hq=mhq**2).NNLO()
             # singlet reg
             yad = fl_s.reg(x, fl_s.args["reg"])
-            my = cf.CLq_2_m_reg(x, self.Q, p, NF) / e_h**2
-            assert_allclose(my, yad, rtol=6e-2)
-
+            my = cf.CLq_2_m_reg(x, self.Q, p, NF, use_grids) / e_h**2
+            assert_allclose(my, yad)
