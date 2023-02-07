93b5426b1cbc64f0a05fc0226aece6658aeb709e
Giacomo Magni <giac.magni@gmail.com>
2023-02-07 17:16:46 +0100

    small fixes

--- tests/test_TildeCoeffFunc.py
+++ tests/test_TildeCoeffFunc.py
@@ -59,11 +59,11 @@ class Test_F2:
             p = np.array([mhq, self.Q, e_h])
             my_grid = tf.Cg_2_til_reg(x, self.Q, p, h_id)
             my = tf.Cg_2_til_reg(x, self.Q, p, h_id, use_analytic=True)
-            assert_allclose(my, my_grid, rtol=7e-4)
+            assert_allclose(my, my_grid, rtol=4e-4)
 
             my_grid = tf.Cq_2_til_reg(x, self.Q, p, h_id)
             my = tf.Cq_2_til_reg(x, self.Q, p, h_id, use_analytic=True)
-            assert_allclose(my, my_grid, rtol=4e-4)
+            assert_allclose(my, my_grid, rtol=7e-7)
 
 
     def test_n3lo(self):
@@ -87,7 +87,7 @@ class Test_FL:
             p = np.array([mhq, self.Q, e_h])
             my_grid = tf.CLg_2_til_reg(x, self.Q, p, h_id)
             my = tf.CLg_2_til_reg(x, self.Q, p, h_id, use_analytic=True)
-            assert_allclose(my, my_grid, rtol=4e-4)
+            assert_allclose(my, my_grid, rtol=3e-4)
 
             my_grid = tf.CLq_2_til_reg(x, self.Q, p, h_id)
             my = tf.CLq_2_til_reg(x, self.Q, p, h_id, use_analytic=True)
