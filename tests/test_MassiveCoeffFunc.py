d3a59398d93f0267ba2dd2c232fdc0e5dac86ffc
Giacomo Magni <giac.magni@gmail.com>
2023-02-15 10:26:11 +0100

    Add more tests on Nic normalization

--- tests/test_MassiveCoeffFunc.py
+++ tests/test_MassiveCoeffFunc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pathlib
 from numpy.testing import assert_allclose
 from test_MasslessCoeffFunc import MockESF
 from yadism.coefficient_functions.heavy import f2_nc, fl_nc
@@ -6,6 +7,10 @@ from yadism.coefficient_functions.heavy import f2_nc, fl_nc
 from dis_tp import MassiveCoeffFunc as cf
 from dis_tp.Initialize import Initialize_all
 from dis_tp.parameters import charges, default_masses, initialize_theory
+from dis_tp.ReadTxt import readND
+
+from scipy.interpolate import interp2d
+from dis_tp import Initialize as Ini
 
 h_id = 5
 mhq = default_masses(h_id)
@@ -14,6 +19,7 @@ e_h = charges(h_id)
 p = np.array([mhq, e_h])
 Initialize_all(h_id)
 
+here = pathlib.Path(__file__).parent
 
 class Test_F2:
     xs = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.456, 0.7]
@@ -71,3 +77,61 @@ class Test_FL:
             yad = fl_s.reg(x, fl_s.args["reg"])
             my = cf.CLq_2_m_reg(x, self.Q, p, h_id) / e_h**2
             assert_allclose(my, yad)
+
+
+
+class TestNic:
+    """Here we test that Niccolo' code follow the same normalization."""
+    # NOTE: the accuracy is bad but these grids are not used...
+    # TODO: how can we trust Niccolo' grids?
+    xs = [0.0001, 0.0123, 0.456]
+    Q = 10
+    nf = 5
+    c2g = np.array(
+        readND(here / f"grids/C2g.dat")
+    )
+    c2q = np.array(
+        readND(here / f"grids/C2q.dat")
+    )
+    cLg = np.array(
+        readND(here / f"grids/CLg.dat")
+    )
+    cLq = np.array(
+        readND(here / f"grids/CLq.dat")
+    )
+    c2g = interp2d(Ini.ZList, Ini.QList, c2g, kind="quintic")
+    c2q = interp2d(Ini.ZList, Ini.QList, c2q, kind="quintic")
+    cLg = interp2d(Ini.ZList, Ini.QList, cLg, kind="quintic")
+    cLq = interp2d(Ini.ZList, Ini.QList, cLq, kind="quintic")
+
+    def test_Lg(self):
+        dis_tp = []
+        my = []
+        for x in self.xs:
+            dis_tp.append(cf.CLg_2_m_reg(x, self.Q, p, h_id) / e_h**2)
+            my.append(self.cLg(x, self.Q)[0])
+        assert_allclose(my, dis_tp, rtol=1e-3)
+
+    def test_Lq(self):
+        dis_tp = []
+        my = []
+        for x in self.xs:
+            dis_tp.append(cf.CLq_2_m_reg(x, self.Q, p, h_id) / e_h**2)
+            my.append(self.cLq(x, self.Q)[0])
+        assert_allclose(my, dis_tp, rtol=1e-1)
+    
+    def test_2g(self):
+        dis_tp = []
+        my = []
+        for x in self.xs:
+            dis_tp.append(cf.Cg_2_m_reg(x, self.Q, p, h_id) / e_h**2)
+            my.append(self.c2g(x, self.Q)[0])
+        assert_allclose(my, dis_tp, rtol=9e-4)
+
+    def test_2q(self):
+        dis_tp = []
+        my = []
+        for x in self.xs:
+            dis_tp.append(cf.Cq_2_m_reg(x, self.Q, p, h_id) / e_h**2)
+            my.append(self.c2q(x, self.Q)[0])
+        assert_allclose(my, dis_tp, rtol=3e-1)
