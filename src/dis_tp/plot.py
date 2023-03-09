408790bd32176e98d4e67e50a0efd487301ac974
andreab1997 <andreab1997@yahoo.it>
2023-03-09 12:24:00 +0100

    Fix plot

--- src/dis_tp/plot.py
+++ src/dis_tp/plot.py
@@ -179,22 +179,17 @@ class Plot:
                 "": 1.0,
                 "_mub=2mb": 2.0,
             }
-            shifts_moving = {"_mub=05mb": 0.02, "": 0.0, "_mub=2mb": 0.35}
             for sv in ordered_result_R:
                 res_plot_tmp = [res for res in ordered_result_R[sv] if res["x"] == x]
                 res_plot = [
-                    res["res"]
-                    if res["q"] > (shifts[sv] * mass) + shifts_moving[sv]
-                    else np.nan
+                    res["res"] if res["q"] >= (shifts[sv] * mass) else np.nan
                     for res in res_plot_tmp
                 ]
                 res_plot_R[sv] = res_plot
             for sv, fo_sv in zip(ordered_result_M, sv_FO_coll):
                 res_plot_tmp = [res for res in ordered_result_M[sv] if res["x"] == x]
                 res_plot = [
-                    res["res"]
-                    if res["q"] > (shifts[sv] * mass) + shifts_moving[sv]
-                    else res_FO
+                    res["res"] if res["q"] >= (shifts[sv] * mass) else res_FO
                     for res, res_FO in zip(res_plot_tmp, fo_sv)
                 ]
                 res_plot_M[sv] = res_plot
