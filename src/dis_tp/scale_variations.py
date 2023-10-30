e38ad7d635c4dc399640c847b2ace3db7eb362ec
andreab1997 <andreab1997@yahoo.it>
2023-10-30 14:52:06 +0100

    Move the scale variations module

--- src/dis_tp/scale_variations.py
+++ src/dis_tp/scale_variations.py
@@ -19,19 +19,17 @@ def apply_sv_kernel(
 ):
     if np.isclose(muf_ratio, 1.0) and np.isclose(mur_ratio, 1.0):
         return ingredients[0]
+    if len(ingredients) != (order + 1):
+        raise IncorrectNumberOfIngredients
     LF = np.log(muf_ratio)
     LR = np.log(mur_ratio)
     if order == 0:
-        if len(ingredients) != (order + 1):
-            raise IncorrectNumberOfIngredients
         return ingredients[0]
     elif order == 1:
-        if len(ingredients) != (order + 1):
-            raise IncorrectNumberOfIngredients
         # return ingredients[0] - ingredients[-1](z, Q, p, nf) * gamma[0] * LF - ingredients[-1](z, Q, p, nf) * beta[0] * LR * m
         return ingredients[0]
     elif order == 2:
-        if len(ingredients) != (order + 1):
-            raise IncorrectNumberOfIngredients
         # return ingredients[0] - LR*(m*beta[1]*ingredients[-1](z, Q, p, nf) + (m+1)*beta[0]*ingredients[-2](z, Q, p, nf)) + 0.5*(LR**2)*m*(m+1)*beta[0]*beta[0]*ingredients[-1](z, Q, p, nf) - LF*gamma[1]*ingredients[-1](z, Q, p, nf) - LF*gamma[0]*ingredients[-2](z, Q, p, nf) +LF*LR*(m+1)*beta[0]*gamma[0]*ingredients[-1](z, Q, p, nf) + 0.5*(LF**2)*(gamma[0]*gamma[0] - beta[0]*gamma[0])*ingredients[-1](z, Q, p, nf)
+        return ingredients[0]
+    elif order == 3:
         return ingredients[0]
